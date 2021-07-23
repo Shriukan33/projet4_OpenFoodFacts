bc1353fe208f9254a0656a68c20926da4541c674
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2021-07-23 16:37:48 +0200

    Add data retrieval from OpenFoodFacts API.

---
+++ src/off_projet_5.py
@@ -0,0 +1,9 @@
+import requests
+
+# Query to OpenFoodFacts API
+url = "https://fr.openfoodfacts.org/cgi/search.pl?action=process&sort_by=popularity&page_size=10&page=1&sort_by=unique_scans_n&json=true&fields=product_name,nutriscore_grade,code,url,categories,stores,purchase_places,pnns_groups_1,pnns_groups_2&coutries=france" # noqa
+headers = {"User-Agent": "Projet5 - Linux/ubuntu - Version 1.0"}
+r = requests.get(url, headers=headers)
+
+# Turn json response into a dict.
+json_data = r.json()

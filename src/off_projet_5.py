8b5f8c2a88450f131e5040553b65334927ad0d08
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2021-07-29 15:39:51 +0200

    Refactored main file to improve on readability.

--- src/off_projet_5.py
+++ src/off_projet_5.py
@@ -1,8 +1,12 @@
-import requests
-
-from utils import create_database_if_doesnt_exist
+from utils import (create_database_if_doesnt_exist, create_tables,
+                   request_to_data, insert_data_into_table,
+                   get_json_data_from_api)
 
 # Query to OpenFoodFacts API
 json_data = get_json_data_from_api()
 
 create_database_if_doesnt_exist()
+create_tables()
+
+data = request_to_data(json_data)
+insert_data_into_table("product", data)

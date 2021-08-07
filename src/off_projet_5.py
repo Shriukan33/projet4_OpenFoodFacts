fdc2b17c25ec41be643784208f626452c4c14868
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2021-08-07 10:51:27 +0200

    Create db_setup.py class to tidy the code
    db_setup will handle the creation of the database and the tables
    at script startup.

--- src/off_projet_5.py
+++ src/off_projet_5.py
@@ -1,20 +1,10 @@
-from utils import (check_database_existence, create_database_if_doesnt_exist,
-                   create_tables, request_to_data, insert_data_into_table,
-                   get_json_data_from_api, display_products_from_category,
+from utils import (display_products_from_category,
                    display_alternatives_of_product)
-from settings_local import DB_NAME
+
+from db_setup import SetupDatabase
 
 
-database_already_exists = check_database_existence(DB_NAME)
-
-if not database_already_exists:
-    json_data = get_json_data_from_api()
-
-    create_database_if_doesnt_exist()
-    create_tables()
-
-    data = request_to_data(json_data)
-    insert_data_into_table("product", data)
+setup = SetupDatabase()
 
 print("""
 Que voulez vous faire ?
